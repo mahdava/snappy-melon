87c1ac197c980e2e6eac7e0b8e62c12cba4f6f10
Lucia <39827122+mahdava@users.noreply.github.com>
2024-06-03 00:33:59 +0300

    Change settings to Next.js to render with correct links

---
+++ docs/_next/static/chunks/app/page-4e80e0f4df7cdcbd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,222,301],{4080:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=4080)}),_N_E=n.O()}]);
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/snappy-melon",
+  distDir: "docs",
 };
 
 export default nextConfig;
